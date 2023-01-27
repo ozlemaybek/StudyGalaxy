54f7c69c2ed692fa880a56f752401f864d9a831c
Özlem Katibe AYBEK <ozlemkatibeaybek@gmail.com>
2023-01-28 01:55:52 +0300

    Project updated.

--- app/src/main/java/com/example/studygalaxy/view/MainActivity.kt
+++ app/src/main/java/com/example/studygalaxy/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.studygalaxy
+package com.example.studygalaxy.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.studygalaxy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
