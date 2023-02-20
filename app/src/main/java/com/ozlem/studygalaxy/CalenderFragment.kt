132943c636aa13ef344088c320da96f9cf3c987f
Özlem Katibe AYBEK <ozlemkatibeaybek@gmail.com>
2023-02-20 04:45:42 +0300

    Project updated.

--- app/src/main/java/com/ozlem/studygalaxy/CalenderFragment.kt
+++ app/src/main/java/com/ozlem/studygalaxy/CalenderFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Calender_Fragment : Fragment() {
+class CalenderFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/ozlem/studygalaxy/view/MainActivity2.kt
+++ app/src/main/java/com/ozlem/studygalaxy/view/MainActivity2.kt
@@ -2,6 +2,8 @@ package com.ozlem.studygalaxy.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ozlem.studygalaxy.*
 import com.ozlem.studygalaxy.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -11,7 +13,43 @@ class MainActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // view binding için :
         binding2 = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding2.root)
+
+        // İlk açıldığında home page'i göstermek için:
+        replaceFragment(HomeFragment())
+
+        // bottom navigation bar'ın listener'ını yazalım:
+        binding2.bottomNavigationViewId.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                // Burada sol tarafta bottom_navigation_menu.xml'deki id'leri kullanıyoruz,
+                // Sağ tarafta ise Fragment'ların dosya isimlerini kullanıyoruz:
+                R.id.statistics_icon_id -> replaceFragment(StatisticsFragment())
+                R.id.goals_icon_id -> replaceFragment(GoalsFragment())
+                R.id.home_icon_id -> replaceFragment(HomeFragment())
+                R.id.calender_icon_id -> replaceFragment(CalenderFragment())
+                R.id.profile_icon_id -> replaceFragment(ProfileFragment())
+
+                else ->{ }
+
+            }
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        // activity_main2.xml'deki frame layout'un id'si: frameLayout2ID :
+        fragmentTransaction.replace(R.id.frameLayout2ID,fragment)
+        fragmentTransaction.commit()
+
+    }
+
 }
