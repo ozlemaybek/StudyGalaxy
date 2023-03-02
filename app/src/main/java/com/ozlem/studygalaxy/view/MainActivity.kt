12edc67c8d09ea227714390efbf862533ed1f988
Özlem Katibe AYBEK <ozlemkatibeaybek@gmail.com>
2023-03-02 03:54:14 +0300

    Project updated.

--- app/src/main/java/com/ozlem/studygalaxy/view/MainActivity.kt
+++ app/src/main/java/com/ozlem/studygalaxy/view/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.ozlem.studygalaxy.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.get
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ozlem.studygalaxy.R
+import com.ozlem.studygalaxy.databinding.ActivityMain2Binding
+import com.ozlem.studygalaxy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // view binding için :
+    private lateinit var binding : ActivityMainBinding
+
+    // Bir FirebaseAuth objesi oluşturalım:
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // view binding için :
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Oluşturduğumuz FirebaseAuth objesini initialize edelim:
+        auth = Firebase.auth
 
     }
 
@@ -49,4 +68,35 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.frameLayoutID, signUpFragment).commit()
 
     }
+
+    fun signInOnclick(view : View){
+
+            // Kullanıcının girdiği username'i bir string olarak alalım:
+            val username = binding.plainTextUsernameId.text.toString()
+            val password = binding.plainTextPasswordId.text.toString()
+
+            if(username != "" && password != ""){
+                // Kayıtlı birkullanıcının sign in olması:
+                auth.signInWithEmailAndPassword(username, password) .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        //Kullanıcı şu anda girdi. (current user: güncel kullanıcı)
+                        val currentUser = auth.currentUser?.displayName.toString()
+                        Toast.makeText(applicationContext,"Welcome ${currentUser}", Toast.LENGTH_LONG).show()
+
+                        // sign in işlemi başarılı ise diğer ekrana geçelim:
+                        val intent = Intent(this, MainActivity2::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }.addOnFailureListener{ exception ->
+                    Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+            }
+
+        // sign in işlemi başarılı ise diğer ekrana geçelim:
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
--- app/src/main/java/com/ozlem/studygalaxy/view/SignUpFragment.kt
+++ app/src/main/java/com/ozlem/studygalaxy/view/SignUpFragment.kt
@@ -5,9 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ozlem.studygalaxy.R
+import com.ozlem.studygalaxy.databinding.FragmentSignInBinding
+import com.ozlem.studygalaxy.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    // Bir FirebaseAuth objesi oluşturalım:
+    private lateinit var auth: FirebaseAuth
+    // view binding için:
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +29,33 @@ class SignUpFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        // view binding için:
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // DENEME:
+        // Oluşturduğumuz FirebaseAuth objesini initialize edelim:
+        auth = Firebase.auth
+
+
+
+        // DENEME BİTTİ
+
+        return view
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun signUpOnclick(view: View){
+        // Kullanıcının girdiği username'i bir string olarak alalım:
+        val username = binding.plainTextUsernameId.text.toString()
+        val email = binding.plainTextEmailId.text.toString()
+        val password = binding.plainTextPasswordId.text.toString()
+
+    }
+
 
 }
